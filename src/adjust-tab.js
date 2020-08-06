e8a981539872a87586495d3df97d08b94e6bce47
SlaterRGordon <richard.lluttrell@gmail.com>
2020-08-06 19:54:52 +0000

    added documentation to adjust-tab

--- src/adjust-tab.js
+++ src/adjust-tab.js
@@ -2,12 +2,23 @@ import { config } from '../app.config'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min.js'
 
+/**
+ * Class to render a list of sliders that control the TileSet Filter for Map object
+ */
 class AdjustTab {
+
+  /**
+   * Creates an instance of AdjustTab
+   * @param {Map} mapObj Map to bind controls to
+   */
   constructor(mapObj) {
     this.mapObj = mapObj
   }
 
-
+  /**
+   * Renders AjustTab as a child element in node
+   * @param {HTMLElement} node node in which to render AdjustTab
+   */
   render(node) {
     let formElement = document.createElement('form')
     formElement.setAttribute('action',"#")
@@ -18,6 +29,14 @@ class AdjustTab {
     node.appendChild(formElement)
   }
 
+  /**
+   * Creates a single HTML slider for a given TilesetFilter parameter name
+   * @param {string} parameter paramenter name for slider
+   * @param {string} minValue min value for slider
+   * @param {string} maxValue max value for slider
+   * @param {string} defaultValue default value for slider
+   * @return {HTMLElement} HTML paragraph element containing an HTML5 slider 
+   */
   _createTileAdjustmentSliders(parameter, minValue, maxValue, defaultValue) {
     let slider = document.createElement('p')
     slider.classList.add('range-field')
