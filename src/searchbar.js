e95a6e834eadfc3adaf3f9eae69387b28365a498
SlaterRGordon <richard.lluttrell@gmail.com>
2020-08-10 22:13:31 +0000

    added multipile objects to be searched as well as search history

--- src/searchbar.js
+++ src/searchbar.js
@@ -1,4 +1,7 @@
+import 'leaflet'
+import 'leaflet/dist/leaflet.css'
 import './styles/searchbar.css'
+import { config } from '../app.config'
 
 /**
  * This class is used to render a search bar that allows a user to search
@@ -8,8 +11,12 @@ import './styles/searchbar.css'
  */
 class SearchBar {
   constructor(lMap) {
-    this.lMap = lMap
-    this.searchString = ''
+    this.mapObj = lMap
+    this.searchBoxContent = ''
+    this.layerGroup = L.layerGroup()
+    this.layerGroup.addTo(this.mapObj.lMap)
+    this.searchHistory = JSON.parse(window.localStorage.getItem('searchHistory')) || ['']
+    this.searchHistoryPosition = 0
   }
 
   /**
@@ -20,15 +27,23 @@ class SearchBar {
     let searchBar = document.createElement('div')
     searchBar.classList.add('input-field','col','s10')
 
-    let searchBoxInput = document.createElement('input')
+    let searchBoxInput = document.createElement('textarea')
+    searchBoxInput.classList.add('materialize-textarea')
     searchBoxInput.id = 'searchbox-input'
     searchBoxInput.setAttribute('placeholder', 'Object/Location Search')
     searchBoxInput.addEventListener('keyup', (e) => {
       if (e.key === 'Enter') {
+        this._updateSearchHistory()
         this._performSearch()
         searchBoxInput.value = ''
+      } else if (e.key === 'ArrowUp'){
+        this._incrementSearchHistoryPosition()
+        searchBoxInput.value = this.searchHistory[this.searchHistoryPosition]
+      } else if (e.key === 'ArrowDown'){
+        this._decrementSearchHistoryPosition()
+        searchBoxInput.value = this.searchHistory[this.searchHistoryPosition]
       } else {
-        this.searchString = searchBoxInput.value
+        this.searchBoxContent = searchBoxInput.value
       }
     })
     searchBar.appendChild(searchBoxInput)
@@ -36,13 +51,29 @@ class SearchBar {
     let searchBoxImage = document.createElement('i')
     searchBoxImage.classList.add('material-icons','prefix')
     searchBoxImage.innerText = 'search'
-    searchBar.addEventListener('click', () => {
-      this._performSearch()
-      searchBoxInput.value = ''
-    })
+    // searchBar.addEventListener('click', () => {
+    //   this._performSearch()
+    //   searchBoxInput.value = ''
+    // })
     searchBar.appendChild(searchBoxImage)
     
     node.appendChild(searchBar)
+  }
+
+  _decrementSearchHistoryPosition() {
+    if (this.searchHistoryPosition > 0) this.searchHistoryPosition--
+  }
+
+  _incrementSearchHistoryPosition() {
+    if (this.searchHistoryPosition < this.searchHistory.length - 1) this.searchHistoryPosition++
+  }
+
+  _updateSearchHistory() {
+    this.searchHistory.splice(1, 0, this.searchBoxContent)
+    if (this.searchHistory.length > config.searchHistoryLength) {
+      this.searchHistory.length = config.searchHistoryLength
+    }
+    window.localStorage.setItem('searchHistory', JSON.stringify(this.searchHistory))
   }
 
   /**
@@ -50,14 +81,17 @@ class SearchBar {
   * the searchMarker in the map object. Otherwise notifies the user with a toast. 
   */
   async _performSearch() {
-    if (this.searchString === '') {
-      this.lMap.clearSearchMarker()
+    if (this.searchBoxContent === '') {
+      this.layerGroup.clearLayers()
     } else {
-      try {
-        let queryResults = await this._queryNameResolver()
-        this.lMap.setSearchMarker(queryResults.name, queryResults.coordinates)
-      } catch (e) {
-        M.toast({html: e.message, classes:'red lighten-2'})
+      this.layerGroup.clearLayers()
+      for (let searchString of this.searchBoxContent.split(/\b\s/)) {
+        try {
+          let queryResults = await this._queryNameResolver(searchString)
+          this.layerGroup.addLayer(this._createSearchMarker(queryResults.name, queryResults.coordinates))
+        } catch (e) {
+          M.toast({html: e.message, classes:'red lighten-2'})
+        }
       }
     }
   }
@@ -66,13 +100,13 @@ class SearchBar {
    * Queries the cadc name resolver service, if a result is returned from the api
    * returns an object containing the name and coordinates of the search results
    */
-  async _queryNameResolver() {
-    let response = await fetch(`https://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/AdvancedSearch/unitconversion/Plane.position.bounds?term=${this.searchString}&resolver=all`)
+  async _queryNameResolver(searchString) {
+    let response = await fetch(`https://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/AdvancedSearch/unitconversion/Plane.position.bounds?term=${searchString}&resolver=all`)
     let resultJSON = await response.json();
     if (resultJSON.resolveStatus === "GOOD") {
       return this._parseNameResolverResults(resultJSON.resolveValue)
     } else {
-      throw new Error('Search Failed')
+      throw new Error(`Search for ${searchString} failed`)
     }
   }
 
@@ -91,6 +125,37 @@ class SearchBar {
       coordinates: [targetDec, targetRA]
     }
   }
+
+  _createSearchMarker(name, coordinates) {
+    let searchMarker = L.marker(this._toLatLng(coordinates), {
+      title: name,
+      icon: this._createMarkerIcon(config.searchMarkerColor)
+    })
+    return searchMarker
+  }
+
+  _toLatLng(coordinates) {
+    let dec = coordinates[0]
+    let ra = coordinates[1];
+    if (ra > 180) { ra = 180 - ra };
+    return L.latLng([dec,ra]);
+  }
+
+   /**
+   * Returns a leaflet marker icon
+   * @param {string} color icon color (red, blue, green, yellow, black)
+   */
+  _createMarkerIcon(color) {
+    return new L.Icon({
+        iconUrl: `https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    })
+  }
+
 }
 
 export default SearchBar
