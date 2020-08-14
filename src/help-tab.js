713c5dda6039546a42e61b3379f81e67fa53d111
SlaterRGordon <richard.lluttrell@gmail.com>
2020-08-14 19:25:00 +0000

    added documentation to HelpTab and moved help.html to assets directory

--- src/help-tab.js
+++ src/help-tab.js
@@ -1,12 +1,23 @@
-import help from './help.html'
+import help from './assets/help.html'
 import './styles/help.css'
 
+/**
+ * Class that renders the contents of the Help Tab. Only really included so it works the same way
+ * as the other 'Tabs'. All it does is render a static html file inside a div.
+ */
 class HelpTab {
+  /**
+   * Replaces contents of node with rendering of HelpTab
+   * @param {HTMLElement} node node to render content in 
+   */
   render(node) {
     node.innerHTML = ''
     node.appendChild(this._createHelpDiv())
   }
 
+  /**
+   * Creates a div populated with the html in help.html
+   */
   _createHelpDiv() {
     let div = document.createElement('div')
     div.classList.add('help-div')
