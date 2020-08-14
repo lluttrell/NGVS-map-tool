7b5ca72cbe73da9797ee3b1a2f575526b1dc4771
SlaterRGordon <richard.lluttrell@gmail.com>
2020-08-14 20:59:43 +0000

    code cleanup. Put marker size and catalog api endpoint in config file

--- app.config.js
+++ app.config.js
@@ -32,6 +32,8 @@ const config = {
       'markerColor' : 'blue'
     }
   ],
+  'catalogMarkerSize': 500,
+  'catalogEndpoint' : 'https://ws-cadc.canfar.net/youcat/sync?LANG=ADQL&FORMAT=csv&QUERY=',
   'skyTileUrl' : 'https://mw1.google.com/mw-planetary/sky/skytiles_v1/{x}_{y}_{z}.jpg',
   'ngvsTileUrl' : 'https://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/data/pub/GSKY/M.{x}.{y}.{z}.png',
   'colors' : ['yellow','red','blue','orange','teal','purple','lightgreen'],
