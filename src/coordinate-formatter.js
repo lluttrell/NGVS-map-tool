41cdf5e3992cb465bdfe5808d3b410c4e497e848
SlaterRGordon <richard.lluttrell@gmail.com>
2020-08-14 21:42:53 +0000

    cleaned up styling

--- src/coordinate-formatter.js
+++ src/coordinate-formatter.js
@@ -5,6 +5,7 @@
  * unsuitable for other uses.
  */
 
+
 /**
  * Takes a right ascension value in decimal format in the range (-180, 360) and returns
  * a rounded decimal format in the range (0,360).
@@ -15,9 +16,9 @@ const decimal_ra_formatter = (num, precision=4) => {
   if (num <= -180 || num >= 360) {
     throw new RangeError('Coordinate must be between -180 and 360.')
   }
-  num = 180 - num;
+  num = 180 - num
   return num.toFixed(precision)
-};
+}
 
 
 /**
@@ -30,8 +31,9 @@ const decimal_dec_formatter = (num, precision=4) => {
   if (num < -90 || num > 90) {
     throw new RangeError('Coordinate must be between -90 and 90')
   }
-  return num.toFixed(precision);
-};
+  return num.toFixed(precision)
+}
+
 
 /**
  * Takes a declination value in decimal format and returns coordinate in
@@ -43,13 +45,14 @@ const dms_formatter = (num, precision=0) => {
   if (num < -90 || num > 90) {
     throw new RangeError('Coordinate must be between -90 and 90')
   }
-  let deg = Math.floor(num) ;
-  let frac = Math.abs(num - deg);
-  let min = Math.floor(frac * 60);
-  let sec = Math.floor(frac * 3600 - min * 60 );
-  let fsec = (frac * 3600 - min * 60 - sec ).toFixed(precision) ;
-  return ("00" + deg).slice(-2) + ":" + ("00" + min).slice(-2) + ":" + ( "00" + sec).slice(-2) + fsec.toString().slice(1, precision + 2);
-};
+  let deg = Math.floor(num)
+  let frac = Math.abs(num - deg)
+  let min = Math.floor(frac * 60)
+  let sec = Math.floor(frac * 3600 - min * 60 )
+  let fsec = (frac * 3600 - min * 60 - sec ).toFixed(precision) 
+  return ("00" + deg).slice(-2) + ":" + ("00" + min).slice(-2) + ":" + ( "00" + sec).slice(-2) + fsec.toString().slice(1, precision + 2)
+}
+
 
 /**
  * Takes a declination value in decimal format and returns coordinate in
@@ -61,14 +64,13 @@ const hms_formatter = (num, precision=0) => {
   if (num <= -180 || num >= 360) {
     throw new RangeError('Coordinate must be between -180 and 360.')
   }
-  num = 180 - num;
-  let hrs = Math.floor(num/15);
-  let min = Math.floor((num/15 - hrs) * 60);
-  let sec = (num/15 - hrs - min/60) * 3600;
-  let wsec = Math.floor(sec);
+  num = 180 - num
+  let hrs = Math.floor(num/15)
+  let min = Math.floor((num/15 - hrs) * 60)
+  let sec = (num/15 - hrs - min/60) * 3600
+  let wsec = Math.floor(sec)
   let fsec = (sec - wsec).toFixed(precision)
-  return ("00" + hrs).slice(-2) + ":" + ("00" + min).slice(-2) + ":" + ("00" + wsec).slice(-2) + fsec.toString().slice(1, precision + 2);
-
+  return ("00" + hrs).slice(-2) + ":" + ("00" + min).slice(-2) + ":" + ("00" + wsec).slice(-2) + fsec.toString().slice(1, precision + 2)
 }
 
 export { decimal_dec_formatter, decimal_ra_formatter, dms_formatter, hms_formatter }
