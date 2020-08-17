70e8319ded97c6c7b1b65b2ac36e460748a78015
SlaterRGordon <richard.lluttrell@gmail.com>
2020-08-17 20:19:11 +0000

    added documentation

--- src/query-tab.js
+++ src/query-tab.js
@@ -2,7 +2,16 @@ import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min.js'
 import CatalogQueryForm from './catalog-query-form'
 
+/**
+ * Class that renders contents of 'Query Catalog' tab for the application. Creates a select menu
+ * with entries for all YouCat catalogs for the application, as well as logic to switch the currently
+ * selected catalog
+ */
 class QueryTab {
+  /**
+   * Creates an instance of QueryTab
+   * @param {Array} catalogList Array containing names of YouCat catalogs to create query forms for
+   */
   constructor(catalogList) {
     this.catalogList = catalogList
     this.catalogQueryFormDiv = document.createElement('div')
@@ -11,6 +20,11 @@ class QueryTab {
     this.currentCatalog = null
   }
 
+  
+  /**
+   * Renders QueryTab as a child of the node element
+   * @param {HTMLElement} node node to render querytab in 
+   */
   render(node) {
     let queryTabDiv = document.createElement('div')
     queryTabDiv.classList.add('container')
@@ -21,9 +35,14 @@ class QueryTab {
       queryTabDiv.appendChild(this.catalogQueryFormDiv)
     }
     node.appendChild(queryTabDiv)
-    
   }
 
+
+  /**
+   * Creates an HTML select menu with entries for each catalog. Event listener swtiches value
+   * of the currentCatalog based on value of select menu
+   * @return {HTMLElement} HTML Select element with entries for each catalog
+   */
   _createSelectMenu() {
     let catalogSelectMenu = document.createElement('select')
     catalogSelectMenu.id = 'query-tab-select-menu'
@@ -45,6 +64,10 @@ class QueryTab {
     return catalogSelectMenu
   }
 
+
+  /**
+   * Changes value of currentcatalog and re-renders the query form for the catalog
+   */
   _changeCatalog() {
     let selectedCatalogName = document.getElementById('query-tab-select-menu').value
     for (let catalog of this.catalogList) {
@@ -55,6 +78,11 @@ class QueryTab {
     this._refreshCatalogQueryFormDiv()
   }
 
+
+  /**
+   * creates a new catalog query form for the selected catalog
+   * @todo Keep objects persistent and just switch objects rather than recreating
+   */
   _refreshCatalogQueryFormDiv() {
     this.currentCatalogQueryForm = new CatalogQueryForm(this.currentCatalog)
     this.catalogQueryFormDiv.innerText = ''
