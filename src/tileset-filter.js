e41c644dc0c245244214a97f1e3e2c307155542d
SlaterRGordon <richard.lluttrell@gmail.com>
2020-08-17 20:26:24 +0000

    added documentation

--- src/tileset-filter.js
+++ src/tileset-filter.js
@@ -1,6 +1,13 @@
 import { config } from '../app.config'
 
+/**
+ * The TilesetFilter stores a group of CSS image propetries (eg saturation) and their values in the form
+ * of a 'filter' object. It is used to generate a list of properties and values that can be used by the leaflet tilefilter extension 
+ */
 class TilesetFilter {
+  /**
+   * Creates a new instance of TilesetFilter. Uses properties as definedi in the app.config.js file
+   */
   constructor() {
     this.filter = config.tilesetFilters
     for (let filterProperty of Object.keys(this.filter)) {
@@ -9,11 +16,20 @@ class TilesetFilter {
     this.stringList = this._getFilterAsList()
   }
 
+  /**
+   * Adjust the value of a peroperty in this filter
+   * @param {string} propertyName property to adjust
+   * @param {string} value value to adjust to
+   */
   adjustFilterProperty(propertyName, value) {
     this.filter[propertyName].currentValue = parseInt(value)
     this.stringList = this._getFilterAsList()
   }
 
+  /**
+   * Returns the filter in string format that can be read by the leaflet tilefilter extension
+   * @return {string} string representation of this.filter
+   */
   _getFilterAsList() {
     let filterList = []
     for (let [filterProperty, propertyInfo] of Object.entries(this.filter)) {
