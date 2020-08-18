503a6a1ff52f0e64854a39012ad9763dfda186b8
SlaterRGordon <richard.lluttrell@gmail.com>
2020-08-18 22:35:23 +0000

    removed unused constructor variable

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ class App {
     const adjustTab = new AdjustTab(this.mapObj)
     adjustTab.render(document.getElementById('adjustment-tab-body'))
 
-    const fitsTab = new FITSTab(this.mapObj, this.fitsmgr, this.catalogList)
+    const fitsTab = new FITSTab(this.mapObj, this.fitsmgr)
     fitsTab.render(document.getElementById('fits-tab-body'))
 
     const helpTab = new HelpTab()
